31a470f664c52c6f0dc4fc144c92f36a9d89f631
sabrina <sabrina_camile2011@hotmail.com>
2020-10-01 17:47:40 -0300

    Calculadora em Java S

---
+++ js/arquivo.js
@@ -0,0 +1,12 @@
+var termo1 = prompt("Digite o primeiro termo da soma:");
+var termo2 = prompt("Digite o segundo termo da soma:");
+
+var num1 = parseFloat(termo1);
+var num2 = parseFloat(termo2);
+
+var soma = num1 + num2;
+
+var resultado = parseInt(soma);
+console.log(resultado);
+
+alert(soma);
